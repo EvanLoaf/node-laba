fb736a0543824e265f7403c281f60e1c6a728476
Ivan Karavai <ikaravai@solvd.com>
2023-08-02 11:53:01 +0300

    chore: add eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	root: true,
+	extends: ['prettier', 'plugin:prettier/recommended'],
+	plugins: ['prettier'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'arrow-body-style': 'off',
+		'prefer-arrow-callback': 'off',
+	},
+	overrides: [
+		{
+			files: ['*.js'],
+			extends: ['plugin:prettier/recommended', 'prettier'],
+			plugins: ['prettier'],
+			rules: {
+				'prettier/prettier': [
+					'error',
+					{
+						tabWidth: 2,
+						useTabs: true,
+						semi: true,
+						singleQuote: true,
+						quoteProps: 'as-needed',
+						trailingComma: 'es5',
+						bracketSpacing: true,
+						bracketSameLine: true,
+						arrowParens: 'avoid',
+						printWidth: 140,
+					},
+				],
+				'arrow-body-style': 'off',
+				'prefer-arrow-callback': 'off',
+				quotes: [
+					'error',
+					'single',
+					{
+						avoidEscape: true,
+						allowTemplateLiterals: false,
+					},
+				],
+				'no-confusing-arrow': ['error', { allowParens: false, onlyOneSimpleParam: true }],
+			},
+		},
+	],
+};
