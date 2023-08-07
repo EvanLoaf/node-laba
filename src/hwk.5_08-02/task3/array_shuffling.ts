7b88a4f5ff9f94f0857d129d24106936d64f504b
Ivan Karavai <ikaravai@solvd.com>
2023-08-07 12:21:39 +0300

    hwk(5.3): implement customShuffle function

---
+++ src/hwk.5_08-02/task3/array_shuffling.ts
@@ -0,0 +1,14 @@
+import { getRandomInt } from '../../util/generation_util';
+
+export function customShuffle(array: any[]) {
+	const shuffledArray: any[] = [...array];
+	const length: number = array.length;
+	for (let i = shuffledArray.length - 1; i >= 0; i--) {
+		const randomIndex: number = getRandomInt(i, length - 1);
+		[shuffledArray[i], shuffledArray[randomIndex]] = [
+			shuffledArray[randomIndex],
+			shuffledArray[i],
+		];
+	}
+	return shuffledArray;
+}
