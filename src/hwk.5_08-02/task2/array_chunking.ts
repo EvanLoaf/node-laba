d89aa8629b3e2e29b176a2d0fbdc8848f7bb5592
Ivan Karavai <ikaravai@solvd.com>
2023-08-07 13:50:26 +0300

    fix(5.2): add explicit types in the chunkArray function

--- src/hwk.5_08-02/task2/array_chunking.ts
+++ src/hwk.5_08-02/task2/array_chunking.ts
@@ -1,7 +1,7 @@
-export function chunkArray(array, chunkSize) {
+export function chunkArray(array: any[], chunkSize: number) {
 	const length: number = array.length;
 	for (let i = 0; i < Math.ceil(length / chunkSize); i++) {
-		const chunk = array.slice(i, i + chunkSize);
+		const chunk: any[] = array.slice(i, i + chunkSize);
 		array.splice(i, chunkSize, chunk);
 	}
 	return array;
