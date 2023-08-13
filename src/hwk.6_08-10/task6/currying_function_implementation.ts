ed93a9344a02f6160296fc10e396e47e9d2559c7
Ivan Karavai <ikaravai@uskoinc.com>
2023-08-13 15:20:57 +0300

    hwk(6.7*): implement currying function with placeholders

--- src/hwk.6_08-10/task6/currying_function_implementation.ts
+++ src/hwk.6_08-10/task6/currying_function_implementation.ts
@@ -7,3 +7,24 @@ export function curry(func: (...args: any[]) => number, arity: number) {
 		}
 	};
 }
+
+export const _ = Symbol('placeholder');
+
+export function curryWithPlaceholders(func: (...args: any[]) => number, arity: number) {
+	return function curried(...args: any[]) {
+		while (args.length < arity) {
+			args.push(_);
+		}
+		if (!args.includes(_)) {
+			return func.apply(this, args);
+		} else {
+			return {
+				args: args,
+				fn: (...nextArgs: any[]): object => {
+					const allArgs: any[] = args.map(arg => (arg === _ ? (nextArgs.length > 0 ? nextArgs.shift() : _) : arg));
+					return curried.apply(this, allArgs);
+				},
+			};
+		}
+	};
+}
