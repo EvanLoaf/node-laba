fd02913b2adea6c5e324373286bb575d7e8da0fc
Ivan Karavai <ikaravai@uskoinc.com>
2023-09-06 17:36:47 +0300

    hwk(9.3): adjust chain promises function to not modify the input function array

--- src/hwk.9_08-30/task3/chain-promises.function.ts
+++ src/hwk.9_08-30/task3/chain-promises.function.ts
@@ -1,7 +1,7 @@
-export function chainPromises(functions: Function[], result: Promise<any> = Promise.resolve()): Promise<any> {
-	if (!functions.length) {
+export function chainPromises(functions: Function[], result: Promise<any> = Promise.resolve(), index = 0): Promise<any> {
+	if (index > functions.length - 1) {
 		return result;
 	}
-	const currentFunction: Function = functions.shift();
-	return currentFunction(result).then((result: any) => chainPromises(functions, result));
+	const currentFunction: Function = functions[index];
+	return currentFunction(result).then((result: any) => chainPromises(functions, result, index + 1));
 }
