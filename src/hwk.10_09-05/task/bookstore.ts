05e8ad7982a72a96df3c53a5df32e4820bd428fa
Ivan Karavai <ikaravai@uskoinc.com>
2023-09-10 07:26:05 +0300

    hwk(10): add a bookstore scenario with comments

---
+++ src/hwk.10_09-05/task/bookstore.ts
@@ -0,0 +1,47 @@
+import { Book } from './entity/book.entity';
+import { User } from './entity/user.entity';
+import { Cart } from './entity/cart.entity';
+import { Order } from './entity/order.entity';
+import { Discount } from './entity/discount.entity';
+
+// Instantiate Book objects
+const book1: Book = new Book('The Great Gatsby', 'F. Scott Fitzgerald', '978-3-16-148410-0', 15.99, 10);
+const book2: Book = new Book('To Kill a Mockingbird', 'Harper Lee', '978-0-06-112008-4', 12.49, 15);
+const book3: Book = new Book('1984', 'George Orwell', '978-0-452-28423-4', 10.99, 20);
+
+// Instantiate User objects
+const john: User = new User('John Doe', 'johndoe@example.com', 'john123');
+const alice: User = new User('Alice Smith', 'alicesmith@example.com', 'alice456');
+
+// Simulate users adding books to their cart
+// A book won't be added if its availability < 1
+const johnsCart: Cart = new Cart();
+johnsCart.addBook(book1);
+johnsCart.addBook(book2);
+johnsCart.addBook(book3);
+johnsCart.removeBook(book3);
+
+const alicesCart: Cart = new Cart();
+alicesCart.addBook(book2);
+alicesCart.addBook(book3);
+
+console.log(`John's cart: ${JSON.stringify(johnsCart.items)}`);
+console.log(`Alice's cart: ${JSON.stringify(alicesCart.items)}`);
+
+// Create a discount code
+const fallDiscountCode: Discount = new Discount('FALL2023', 15); // 15% discount
+
+// Simulate users placing orders and apply discounts
+// _items are accessed using a getter
+const johnsOrder: Order = new Order(john, johnsCart.items, fallDiscountCode);
+const alicesOrder: Order = new Order(alice, alicesCart.items, fallDiscountCode);
+
+// Calculate order totals after discounts
+const johnsTotal: number = johnsOrder.calculateTotalPrice();
+const alicesTotal: number = alicesOrder.calculateTotalPrice();
+
+console.log(`John's total after the discount: ${johnsTotal}`);
+console.log(`Alice's total after the discount: ${alicesTotal}`);
+
+console.log(`Resulting John's order with dependencies: \n${JSON.stringify(johnsOrder, null, 0)}`);
+console.log(`Resulting Alice's order with dependencies: \n${JSON.stringify(alicesOrder, null, 0)}`);
